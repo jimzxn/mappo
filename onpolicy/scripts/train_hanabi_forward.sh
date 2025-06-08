1e1fae1a8fc18640020baa929d5913791fa8b783
jimzxn <tug64330@temple.edu>
2025-06-08 17:29:55 +0900

    update

--- onpolicy/scripts/train_hanabi_forward.sh
+++ onpolicy/scripts/train_hanabi_forward.sh
@@ -14,6 +14,6 @@ do
     CUDA_VISIBLE_DEVICES=0 python train/train_hanabi_forward.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
     --hanabi_name ${hanabi} --num_agents ${num_agents} --seed ${seed} --n_training_threads 1 --n_rollout_threads 1000 \
     --num_mini_batch 1 --episode_length 100 --num_env_steps 10000000000000 --ppo_epoch 15 \
-    --gain 0.01 --lr 7e-4 --critic_lr 1e-3 --hidden_size 512 --layer_N 2 --entropy_coef 0.015 
+    --gain 0.01 --lr 7e-4 --critic_lr 1e-3 --hidden_size 512 --layer_N 2 --entropy_coef 0.015 --wandb_name "mappo" --user_name "jimzhao0422"
     echo "training is done!"
 done
